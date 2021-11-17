93389ea6bbf6caf91e7c6ef869104e039d996d65
ImaiLisa-Ykn <787203836@qq.com>
2021-11-17 16:42:56 +0800

    简易编译器

---
+++ a.c
@@ -0,0 +1,23 @@
+void main()
+{
+  int sum=1,a=2,b=3,c;
+  for(int i=1;i<10;i++){
+  sum=sum+i;
+  for(int i=1;i<10;i++){
+  a=(1+2)*3;
+  }
+  }
+  if(a>b){
+  c=0;
+  if(a>c){
+  c=0;
+  }
+  else {
+  c=1;
+  }
+  }
+  else {
+  c=1;
+  }
+}
+#
---
+++ b.c
@@ -0,0 +1,12 @@
+void main(){
+	int a=1;
+	if (a>1){
+		a=a+1;
+	}
+	else {
+		while (a <3)
+		{
+			a=a+2;
+		}
+	}
+}
